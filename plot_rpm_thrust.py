4078609e3a84923b9cdbfdea8daf9814364fa9c8
Alex Spitzer <aes368@cornell.edu>
2021-12-27 07:53:26 -0500

    Update plot_rpm_thrust.py

--- plot_rpm_thrust.py
+++ plot_rpm_thrust.py
@@ -1,32 +1,27 @@
-import argparse
-import os
-import sys
+#!/usr/bin/env python
 
 import numpy as np
 import matplotlib.pyplot as plt
 
 if __name__ == "__main__":
-    rpm_thrust_coeffs = [
-      ("kotleta2", [ 6.41527685e-06, -2.17608417e-02,  3.97188659e+01], "F60"),
-      ("kotleta3", [ 6.40120200e-06 ,-2.07110888e-02 , 3.45876910e+01], "F60"),
-    ]
-    plt.figure("RPM-thrust curves")
-    plt.title("RPM-thrust curves", fontsize=16)
+  rpm_thrust_coeffs = [
+    ("kotleta-duty", [5.85935238e-08, -1.31076936e-04, 2.73896867e-02], "F60"),
+    ("kotleta-rpm", [5.89225233e-08, -1.37988721e-04, 9.45800797e-02], "F60"),
+  ]
+  plt.figure("RPM-thrust curves")
+  plt.title("RPM-thrust curves", fontsize=16)
 
-    rpm_range = np.arange(0, 18001, 100)
+  rpm_range = np.arange(0, 18001, 100)
 
-    for coeffs in rpm_thrust_coeffs:
-        label = coeffs[0]
+  for coeffs in rpm_thrust_coeffs:
+    label = coeffs[0]
+    thrust_range = [coeffs[1][2] + coeffs[1][1] * rpm + coeffs[1][0] * rpm ** 2 for rpm in rpm_range]
+    plot_style = "-"
+    plt.plot(rpm_range, thrust_range, plot_style, markersize=3, label=label)
 
-        thrust_range = [coeffs[1][2] + coeffs[1][1] * rpm + coeffs[1][0] * rpm ** 2 for rpm in rpm_range]
-        plot_style = "-"
-        if coeffs[2] == "F80":
-            plot_style = "x-"
-        plt.plot(rpm_range, thrust_range, plot_style, markersize=3, label=label)
-
-    plt.xlabel("RPM")
-    plt.ylabel("Thrust")
-    plt.grid(True, axis='y')
-    plt.tight_layout()
-    plt.legend()
-    plt.show()
+  plt.xlabel("RPM")
+  plt.ylabel("Thrust (N)")
+  plt.grid(True, axis='y')
+  plt.tight_layout()
+  plt.legend()
+  plt.show()
